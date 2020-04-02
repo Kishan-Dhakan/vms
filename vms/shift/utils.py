168394398ff1418c88e47c20da4d850660060d19
Kishan-Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>
2020-04-02 20:32:38 +0530

    removed try except
    Undoing a change

--- vms/shift/utils.py
+++ vms/shift/utils.py
@@ -224,10 +224,7 @@ def get_report_list(duration_list, report_list, total_hours):
 
 
 def get_country_by_name(country_name):
-    try:
-        country = Country.objects.get(name=country_name)
-    except Country.DoesNotExist:
-        country = none
+    country = Country.objects.get(name=country_name)
     return country
 
 
